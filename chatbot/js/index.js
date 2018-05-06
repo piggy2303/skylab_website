e41772821120fb493a4eb7f24f2e10131109e617
piggy2303 <33578509+piggy2303@users.noreply.github.com>
2018-05-06 22:39:49 +0700

    Update index.js

--- chatbot/js/index.js
+++ chatbot/js/index.js
@@ -1,79 +1,58 @@
-(function () {
+(function() {
 
     var chat = {
-        
-        init: function () {
+
+        init: function() {
             this.cacheDOM();
             this.bindEvents();
             this.render();
         },
-        cacheDOM: function () {
+        cacheDOM: function() {
             this.$chatHistory = $('.chat-history');
             this.$button = $('button');
             this.$textarea = $('#message-to-send');
             this.$chatHistoryList = this.$chatHistory.find('ul');
         },
-        bindEvents: function () {
+        bindEvents: function() {
             this.$button.on('click', this.addMessage.bind(this));
             this.$textarea.on('keyup', this.addMessageEnter.bind(this));
         },
         messageToSend: '',
         messageResponses: [
-            "Tôi không hiểu  ý bạn",
-            'Chào bạn , rất vui được làm quen với bạn',
-            "Tên mình là SkyBot",
-            "Hiệu trưởng trường Đại học Công nghệ là PGS TS Nguyễn Việt Hà",
-            "Mình được nhóm Skylab tạo ra",
-            "Không chửi bậy nhé :) tao biết mày là ai đấy :)",
-            "Cảm ơn nhé :)",
-            "Địt mẹ mày :) đừng tưởng tao ngu",
-            "ông chủ Triệu :)"
+            // "Tôi không hiểu  ý bạn",
+            // 'Chào bạn , rất vui được làm quen với bạn',
+            // "Tên mình là SkyBot",
+            // "Hiệu trưởng trường Đại học Công nghệ là PGS TS Nguyễn Việt Hà",
+            // "Mình được nhóm Skylab tạo ra",
+            // "Không chửi bậy nhé :) tao biết mày là ai đấy :)",
+            // "Cảm ơn nhé :)",
+            // "Địt mẹ mày :) đừng tưởng tao ngu",
+            // "ông chủ Triệu :)"
         ],
-        
-        processOutput: function (input) {
-            input = input.trim();
-            input = input.toLowerCase();
-            console.log(input);
 
-            if (input.indexOf("chào") != -1) {
-                return 1;
-            } else if (input.indexOf("tên") != -1) {
-                return 2;
-            } else if (input.indexOf("ai") != -1) {
-                if (input.indexOf("bạn là") != -1) {
-                    return 2;
-                } else if (input.indexOf("tạo ra") != -1) {
-                    return 4;
-                } else if (input.indexOf("hiệu trưởng") != -1) {
-                    if (input.indexOf("đại học công nghệ") != -1) {
-                        return 3;
-                    }
-                    return 0;
+        processOutput: function(input) {
+            $.ajax({
+                url: "answer.php",
+                type: "post",
+                async: false,
+                context: this,
+                dataType: "text",
+                data: {
+                    question : input,
+                },
+                success: function(result) {
+                    // con
+                    this.setdata(result, this);
                 }
-                else if(input.indexOf("ngu") != -1){
-                    if (input.indexOf("đại học công nghệ") != -1) {
-                        return 8;
-                    }
-                    return 0;
-                }
-                return 0;
-
-            } else if (input.indexOf("dm") != -1 || input.indexOf("địt") != -1 ) {
-                return 5;
-            } 
-            else if (input.indexOf("tuấn anh") != -1 ) {
-                if(input.indexOf("đẹp trai") != -1 ||input.indexOf("dz") != -1 ){
-                    return 6;
-                }
-                return 7;
-            } 
-            else {
-                return 0;
-            }
+            });
+            return 0;
+        },
+        setdata: function(result) {
+            this.messageResponses[0] = result;
 
         },
 
-        render: function () {
+        render: function() {
             this.scrollToBottom();
             if (this.messageToSend.trim() !== '') {
                 var template = Handlebars.compile($("#message-template").html());
@@ -90,14 +69,14 @@
                 var templateResponse = Handlebars.compile($("#message-response-template").html());
                 var indexOutput = this.processOutput(this.messageToSend);
                 console.log(indexOutput);
-
+                console.log(this.messageResponses)
                 var contextResponse = {
                     // response: this.getRandomItem(this.messageResponses),
                     response: this.messageResponses[indexOutput],
                     time: this.getCurrentTime()
                 };
 
-                setTimeout(function () {
+                setTimeout(function() {
                     this.$chatHistoryList.append(templateResponse(contextResponse));
                     this.scrollToBottom();
                 }.bind(this), 1200);
@@ -106,24 +85,24 @@
 
         },
 
-        addMessage: function () {
+        addMessage: function() {
             this.messageToSend = this.$textarea.val()
             this.render();
         },
-        addMessageEnter: function (event) {
+        addMessageEnter: function(event) {
             // enter was pressed
             if (event.keyCode === 13) {
                 this.addMessage();
             }
         },
-        scrollToBottom: function () {
+        scrollToBottom: function() {
             this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
         },
-        getCurrentTime: function () {
+        getCurrentTime: function() {
             return new Date().toLocaleTimeString().
             replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
         },
-        getRandomItem: function (arr) {
+        getRandomItem: function(arr) {
             return arr[Math.floor(Math.random() * arr.length)];
         }
 
@@ -135,11 +114,11 @@
         options: {
             valueNames: ['name']
         },
-        init: function () {
+        init: function() {
             var userList = new List('people-list', this.options);
             var noItems = $('<li id="no-items-found">No items found</li>');
 
-            userList.on('updated', function (list) {
+            userList.on('updated', function(list) {
                 if (list.matchingItems.length === 0) {
                     $(list.list).append(noItems);
                 } else {
